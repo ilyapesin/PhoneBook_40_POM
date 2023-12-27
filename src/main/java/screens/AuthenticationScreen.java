136b84424c310f04c4be130fc7acb5e03984672a
ilyapesin <pesinilya@gmail.com>
2023-12-27 22:53:40 +0200

    CW21

---
+++ src/main/java/screens/AuthenticationScreen.java
@@ -0,0 +1,13 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AuthenticationScreen extends BaseScreen{
+    public AuthenticationScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+    @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/action_bar']/android.widget.TextView")
+    MobileElement activityViewText;
+}
---
+++ src/main/java/screens/SplashScreen.java
@@ -0,0 +1,19 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SplashScreen extends BaseScreen{
+
+    public SplashScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+
+@FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/version_text']")
+    MobileElement version_text;
+
+    public String getVersion() {
+        return version_text.getText();
+    }
+}
